7a0d12f7ad80424d59b52b6fce0a4a8675b0dfde
Abhayjeet23 <137137987+Abhayjeet23@users.noreply.github.com>
2023-08-23 12:43:54 +0530

    Update avx4.sol

--- avx4.sol
+++ avx4.sol
@@ -9,64 +9,49 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract DegenToken is ERC20, Ownable {
     constructor() ERC20("Degen", "DGN") {}
 
-    function mint(address recipient, uint256 amount) external onlyOwner {
-        _mint(recipient, amount);
+    function generateTokens(address account, uint256 amount) external onlyOwner {
+        _mint(account, amount);
     }
 
-    function transfer(address recipient, uint256 amount) public override returns (bool) {
+    function transferTokens(address recipient, uint256 amount) public returns (bool) {
         _transfer(msg.sender, recipient, amount);
         return true;
     }
 
-    function transferFrom(address from, address recipient, uint256 amount) public override returns (bool) {
+    function transferTokensFrom(address from, address recipient, uint256 amount) public returns (bool) {
         _transfer(from, recipient, amount);
         _approve(from, msg.sender, allowance(from, msg.sender) - amount);
         return true;
     }
 
-    function approve(address spender, uint256 amount) public override returns (bool) {
+    function authorize(address spender, uint256 amount) public returns (bool) {
         _approve(msg.sender, spender, amount);
         return true;
     }
 
-   function groceryitems() external pure returns (string memory) {
-    string memory itemsInfo = "The below items present in store for purchase:\n"
-        "Item 1-shampoo\n"
-        "Item 2-conditioner\n"
-        "Item 3-serum";
-    return itemsInfo;
+    function generalitems() external pure returns (string memory) {
+    console.log("The following general items are available:");
+    console.log("Item A: Notebook");
+   
+    return "The following general items are available:\nItem A: Notebook:";
 }
 
-
-    function reedemTokens(uint8 _userSelection) external payable returns (bool) {
+    function redeemTokens(uint8 _userSelection) external payable returns (bool) {
         if (_userSelection == 1) {
-            require(this.balanceOf(msg.sender) >= 75,"You do not have enough Degan");
+            require(balanceOf(msg.sender) >= 75, "Insufficient Degen Tokens");
             approve(msg.sender, 75);
             transferFrom(msg.sender, owner(), 75);
-            console.log("you have successfully redeemed for medicine");
+            console.log("You have successfully redeemed for medicine");
             return true;
         }
-        else if (_userSelection == 2) {
-            require(this.balanceOf(msg.sender) >= 50,"You do not have enough Degan");
-            approve(msg.sender, 50);
-            transferFrom(msg.sender, owner(), 50);
-            console.log("you have successfully redeemed for bandage");
-            return true;
+    
+        else {
+            console.log("That is not a valid choice");
+            return false;
         }
-        else if (_userSelection == 3) {
-            require(this.balanceOf(msg.sender) >= 25,"You do not have enough Degan");
-            approve(msg.sender, 25);
-            transferFrom(msg.sender, owner(), 25);
-            console.log("you have successfully redeemed for injection");
-            return true;
-    }
-    else {
-        console.log("That is not a valid choice");
-        return false;
-    }
     }
 
-    function burn(uint256 amount) external {
+    function destroyTokens(uint256 amount) external {
         _burn(msg.sender, amount);
     }
 }
